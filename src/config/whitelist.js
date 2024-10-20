ebdadbd25dab60636368a033c3a3d9e23ee8ca1b
Franco Guevara <francoguevara93@gmail.com>
2024-10-20 20:42:05 -0300

    Last Commit

--- src/config/whitelist.js
+++
@@ -1,3 +0,0 @@
-const whitelist = ['http://localhost:5173', 'https://story-dots-frontend.vercel.app/'];
-
-module.exports = { whitelist };
--- src/server/server.js
+++ src/server/server.js
@@ -8,18 +8,18 @@ require("../databaseConnection/databaseConnection");
 const app = express();
 const port = serverConfig.port || 4000;
 
-app.use(
-  cors({
+const whitelist = ['http://localhost:5173','https://story-dots-frontend.vercel.app'];
+
+app.use(cors({
     origin: function (origin, callback) {
       if (!origin || whitelist.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error("Acceso CORS no permitido"));
+        callback(new Error('Acceso CORS no permitido'));
       }
     },
-    credentials: true,
-  })
-);
+    credentials: true
+  }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
